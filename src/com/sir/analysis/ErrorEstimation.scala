5a3f5aa2fb09fe4322bbb57cae5f551e19472c1f
justdoitqxb <justdoittwo@163.com>
2016-04-15 19:18:05 +0800

    qxb

--- src/com/sir/analysis/ErrorEstimation.scala
+++ src/com/sir/analysis/ErrorEstimation.scala
@@ -17,8 +17,8 @@ object ErrorEstimation {
     val positive = labelAndPreds.filter(r => 0.0 == r._2 ).count.toDouble
     val negitive = labelAndPreds.filter(r => 1.0 == r._2 ).count.toDouble
     val good = labelAndPreds.filter(r => r._1 == r._2).count.toDouble
-    val posgood = labelAndPreds.filter(r => 0.0 == r._2 & 0.0 == r._2).count.toDouble
-    val neggood = labelAndPreds.filter(r => 1.0 == r._2 & 1.0 == r._2).count.toDouble
+    val posgood = labelAndPreds.filter(r => 0.0 == r._1 & 0.0 == r._2).count.toDouble
+    val neggood = labelAndPreds.filter(r => 1.0 == r._1 & 1.0 == r._2).count.toDouble
     val accRate = good / (total + 0.01)
     val sensitivity = posgood / (positive + 0.01)
     val specificity = neggood / (negitive + 0.01)
